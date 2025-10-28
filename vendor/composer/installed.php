2c7cfe0a437eebc451f4cc705795e40dbff2912f
Ayanfe <ayanfe.sogelola@gmail.com>
2025-10-28 23:21:06 +0100

    New changes

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'ayanfeoluwa/stage-2-twig',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5625c7c737d1e8689c49c40f6bec1f8e1920590',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'ayanfeoluwa/stage-2-twig' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5625c7c737d1e8689c49c40f6bec1f8e1920590',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
